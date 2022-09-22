c40558d254249cb38f54f28f9175b81f94e154eb
developerUmair <umairmak143@gmail.com>
2022-09-22 15:21:33 +0500

    Basic authentication using firebase with sigInWithPop and signInWithGoogleRedirect

--- src/routes/sign-in/Sign-in.component.js
+++ src/routes/sign-in/Sign-in.component.js
@@ -1,19 +1,36 @@
 import React from "react";
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
 import {
-  createUserDocumentFromAuth,
+  auth,
+  signInWithGoogleRedirect,
   signInWithGooglePopup,
+  createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
+  useEffect(() => {
+    (async () => {
+      const response = await getRedirectResult(auth);
+      if (response) {
+        const userDocRef = await createUserDocumentFromAuth(response.user);
+      }
+    })();
+  }, []);
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
     // console.log(response);
   };
+
   return (
     <>
       <h1>Sign in page</h1>
-      <button onClick={logGoogleUser}>Sign in with google</button>
+      <button onClick={logGoogleUser}>Sign in with google pop</button>
+      <button onClick={signInWithGoogleRedirect}>
+        Sign in with google redirect
+      </button>
     </>
   );
 };
